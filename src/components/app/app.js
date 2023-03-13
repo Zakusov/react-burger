34cf27cd62cb7f9d107e55002ef74bb0c6c0a828
Sergey Zakusov <sergey.zakusov@gmail.com>
2023-03-13 22:37:02 +0300

    Вынес обращение к API в burger-api.js + добавил проверку ответа сервера

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,37 +2,29 @@ import React from 'react';
 import AppHeader from '../app-header/app-header.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.js';
+import {getIngredients} from '../../utils/burger-api';
 
 function App() {
-    const ingredientsUrl = 'https://norma.nomoreparties.space/api/ingredients';
-    const [state, setState] = React.useState({
-        loading: false,
-        hasError: false,
-        data: []
-    });
+    const [error, setError] = React.useState(false);
+    const [ingredients, setIngredients] = React.useState([]);
 
     React.useEffect(() => {
-        const getBurgerData = async () => {
-            setState({...state, loading: true});
-            try {
-                const res = await fetch(`${ingredientsUrl}`);
-                const data = await res.json();
-                setState({...state, data: data.data, loading: false});
-            } catch (error) {
-                setState({...state, hasError: true});
-            }
-        }
-        getBurgerData();
+        getIngredients().then((res) => {
+            setIngredients(res.data);
+        }).catch((e) => {
+            setError(true);
+            console.log(e)
+        });
     }, [])
 
     return (
         <>
-            {state.hasError && <div>Ошибка выполнения запроса ...</div>}
+            {error && <div>Упс! Похоже, закончились ингридиенты... Попробуйте зайти позже.</div>}
             <AppHeader/>
             <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
-                <BurgerIngredients data={state.data}/>
+                <BurgerIngredients data={ingredients}/>
                 <div className='pt-25'>
-                    <BurgerConstructor arr={state.data}/>
+                    <BurgerConstructor arr={ingredients}/>
                 </div>
             </div>
         </ >
