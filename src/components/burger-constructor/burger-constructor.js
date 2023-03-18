ead32c4294934f49793ad6fff07ac59c1c1dd834
Sergey Zakusov <sergey.zakusov@gmail.com>
2023-03-18 23:18:12 +0300

    Вернул название булок.

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -53,7 +53,7 @@ const BurgerConstructor = () => {
                         <ConstructorElement
                             type="top"
                             isLocked={true}
-                            text="Краторная булка N-200i"
+                            text="Краторная булка N-200i (верх)"
                             price={20}
                             thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}/>
                     </li>
@@ -74,7 +74,7 @@ const BurgerConstructor = () => {
                         <ConstructorElement
                             type="bottom"
                             isLocked={true}
-                            text="Краторная булка N-200i"
+                            text="Краторная булка N-200i (низ)"
                             price={20}
                             thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}/>
                     </li>
