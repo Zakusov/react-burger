54b013ca930cd328427f99be0ca431ff14d14e5f
Sergey Zakusov <sergey.zakusov@gmail.com>
2023-04-12 02:07:30 +0300

    Заменил ProtectedRoute

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
+import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {ProvideAuth} from "../../utils/auth";
-import {ProtectedRouteElement} from "../protected-route/protected-route";
 import {MainPage} from "../../pages/main-page";
 import {LoginPage} from "../../pages/login-page";
 import {RegisterPage} from "../../pages/register-page";
@@ -9,8 +9,8 @@ import {ResetPasswordPage} from "../../pages/reset-password";
 import {ProfilePage} from "../../pages/profile-page";
 import {NotFoundPage} from "../../pages/not-found-page";
 import {useDispatch} from "react-redux";
-import React from "react";
 import {loadIngredients} from "../../services/actions/ingredients-actions";
+import {ProtectedRoute} from "../protected-route/protected-route";
 
 export default function App() {
     const dispatch = useDispatch();
@@ -26,11 +26,14 @@ export default function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<MainPage/>}/>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
-                    <Route path="/reset-password" element={<ResetPasswordPage/>}/>
-                    <Route path="/profile" element={<ProtectedRouteElement element={<ProfilePage/>}/>}/>
+                    <Route path="/login" element={<ProtectedRoute anonymous={true}><LoginPage/></ProtectedRoute>}/>
+                    <Route path="/register"
+                           element={<ProtectedRoute anonymous={true}><RegisterPage/></ProtectedRoute>}/>
+                    <Route path="/forgot-password"
+                           element={<ProtectedRoute anonymous={true}><ForgotPasswordPage/></ProtectedRoute>}/>
+                    <Route path="/reset-password"
+                           element={<ProtectedRoute anonymous={true}><ResetPasswordPage/></ProtectedRoute>}/>
+                    <Route path="/profile" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
                     <Route path="/ingredients/:id" element={<MainPage/>}/>
                     <Route path="*" element={<NotFoundPage/>}/>
                 </Routes>
--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,24 +1,32 @@
-import {Navigate} from 'react-router-dom';
-import {useEffect, useState} from 'react';
+import {Navigate, useLocation} from 'react-router-dom';
+import {useEffect} from 'react';
 import {useAuth} from "../../utils/auth";
 
 /**
- * Если пользователь не авторизован и пытается попасть на защищённый маршрут, то переадресовываем его на маршрут /login.
- *
- * @param element элемент для авторизованного пользователя.
+ * Если пользователь не авторизован и пытается попасть на защищённый маршрут, то переадресовываем его на страницу входа.
+ * Если пользователь авторизован и пытается попасть на страницу для неавторизованных пользователей, то возвращаем его на предыдущую страницу.
  */
-export const ProtectedRouteElement = ({element}) => {
-    const {getUser, ...auth} = useAuth();
-    const [isUserLoaded, setUserLoaded] = useState(false);
+export const ProtectedRoute = ({children, anonymous = false}) => {
+    const auth = useAuth();
 
     useEffect(() => {
-        getUser().then(() => setUserLoaded(true))
-            .catch((error) => console.log(error));
-    }, [getUser]);
+        auth.getUser().catch((error) => console.log(error));
+    }, [auth]);
 
-    if (!isUserLoaded) {
-        return null;
+    const location = useLocation();
+    // Если разрешен неавторизованный доступ, а пользователь авторизован...
+    if (anonymous && auth.user) {
+        // ...то отправляем его на предыдущую страницу
+        const from = location.state?.from || '/';
+        return <Navigate to={from}/>;
     }
 
-    return auth.user ? element : <Navigate to="/login" replace/>;
+    // Если требуется авторизация, а пользователь не авторизован...
+    if (!anonymous && !auth.user) {
+        // ...то отправляем его на страницу логин
+        return <Navigate to="/login" state={{from: location}}/>;
+    }
+
+    // Если все ок, то рендерим внутреннее содержимое
+    return children;
 }
