821f967778b5de7d43db0c32750d8a178fa4a6f2
Sergey Zakusov <sergey.zakusov@gmail.com>
2023-05-15 23:37:00 +0300

    Типизация ingredientsReducer

--- src/services/actions/ingredients.ts
+++ src/services/actions/ingredients.ts
@@ -1,5 +1,24 @@
 import {getIngredientsRequest} from "../../utils/burger-api";
 import {GET_INGREDIENTS, GET_INGREDIENTS_FAILED, GET_INGREDIENTS_SUCCESS} from "../constants/ingredients";
+import {IngredientType} from "../types/data";
+
+export interface IGetIngredientsAction {
+    readonly type: typeof GET_INGREDIENTS;
+}
+
+export interface IGetIngredientsSuccessAction {
+    readonly type: typeof GET_INGREDIENTS_SUCCESS;
+    readonly ingredients: Array<IngredientType>;
+}
+
+export interface IGetIngredientsFailedAction {
+    readonly type: typeof GET_INGREDIENTS_FAILED;
+}
+
+export type TIngredientsActions =
+    | IGetIngredientsAction
+    | IGetIngredientsSuccessAction
+    | IGetIngredientsFailedAction;
 
 export function loadIngredients() {
     // @ts-ignore
